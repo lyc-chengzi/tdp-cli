c296436bb2755932ebeaa2699e4994f9b9dfb2e6
liuyc14 <liuyc14@lenovo.com>
2022-02-15 16:53:56 +0800

    读取json文件动态写page文件

--- bin/create.js
+++ bin/create.js
@@ -2,12 +2,17 @@ import { program } from "commander";
 import donwload from '../lib/download.js';
 // import { exec } from "node:child_process";
 import chalk from "chalk";
+import createPages from '../lib/createPages.js';
+
+let APPID = '';
 
 program
     .argument('<projectName>', 'created project name')
+    .option('-id, --app-id <appId>', 'input a application id', "")
     .option('-vv, --vue-version <version>', 'choose a vue version', "2")
     .action((projectName) => {
-        // const options = program.opts();
+        const options = program.opts();
+        APPID = options.appId;
         donwload(projectName, downloadSuccess);
     });
     // .requiredOption('-p, --project-name <projectName>', 'the project name');
@@ -20,7 +25,8 @@ program.parse(process.argv);
  */
 function downloadSuccess(destPath) {
     console.log(chalk.greenBright('项目创建完成'));
-    console.log(chalk.cyan('第一步：执行 npm login登录私有npm仓库'));
-    console.log(chalk.cyan('第二步：执行 npm install安装所有依赖'));
-    console.log(chalk.cyan('第三步：执行 npm run serve启动项目并预览'));
+    createPages(APPID, destPath);
+    // console.log(chalk.cyan('第一步：执行 npm login登录私有npm仓库'));
+    // console.log(chalk.cyan('第二步：执行 npm install安装所有依赖'));
+    // console.log(chalk.cyan('第三步：执行 npm run serve启动项目并预览'));
 }

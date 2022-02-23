03593fbfa4f2e7cc3e70d38554cf677a134393bb
liuyc14 <liuyc14@lenovo.com>
2022-02-23 17:39:43 +0800

    添加父类默认方法

--- src/lib/model/nodeBase.ts
+++ src/lib/model/nodeBase.ts
@@ -11,4 +11,16 @@ export default class NodeBase {
     toString(level: number) {
         return '';
     }
+    // 向data中写入代码
+    toData() {
+        return '';
+    }
+    // 向methods中写入代码
+    toMethods() {
+        return '';
+    }
+    // 向mounted中写入代码
+    toMounted() {
+        return '';
+    }
 }
